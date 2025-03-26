9ca7b283d2807a157f10e456aa6e3777177748db
Luis Fernando Cajigas <luis.cajigas@campusucc.edu.co>
2025-03-26 04:21:57 +0000

    Ingresando nuevos archivos a src y tests

---
+++ src/googletrans.py
@@ -0,0 +1,11 @@
+from googletrans import Translator
+
+def traducir_ingles_espanol(texto):
+    traductor = Translator()
+    traduccion = traductor.translate(texto, src='en', dest='es')
+    return traduccion.text
+
+# Ejemplo de uso
+texto_ingles = "Hello, how are you?"
+texto_traducido = traducir_ingles_espanol(texto_ingles)
+print(f"Traducción: {texto_traducido}")
